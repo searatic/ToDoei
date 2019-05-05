2e9c30f52ee151fe84d112cd1b4d52c191570bf8
Leonard Cratic <searatic@gmail.com>
2019-05-04 19:50:07 -0700

    Convert to MVC design pattern

---
+++ ToDoei/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoei
+//
+//  Created by Leonard Cratic on 5/4/19.
+//  Copyright © 2019 Searatic Consulting Technologies, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
